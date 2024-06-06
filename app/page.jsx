ac8022ddbd2117106e627fe37680f961bc6ef83f
Ovesh101 <Oveshmulla786@gmail.com>
2024-06-06 09:47:56 +0530

    restor some of the feature

--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,4 @@
-"use client";
+"use client"
 import Feed from "@components/Feed"
 import { Suspense } from "react"
 
--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,4 +1,4 @@
-"use client"
+
 import { useState , useEffect } from "react"
 
 import PromptCardList from "./PromptCardList";
